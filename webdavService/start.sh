79ee785f478455e998e1b0cf53c89e76118df8bd
Colinas Maoling <colinas.maoling@t-online.de>
2016-01-02 12:31:00 +0100

    Update start.sh

--- webdavService/start.sh
+++ webdavService/start.sh
@@ -1,5 +1,9 @@
 echo $WEBDAV
-echo "$WEBDAV "`cat /webdav/secret` > ~/.davfs/secrets
-cat ~/.davfs/secrets
-mkdir mnt_webdav
-mount -t davfs $WEBDAV /mnt_webdav 
+if [ -e /webdav/secret ]; then 
+  echo "$WEBDAV "`cat /webdav/secret` > ~/.davfs/secrets
+  cat ~/.davfs/secrets
+  mkdir mnt_webdav
+  mount -t davfs $WEBDAV /mnt_webdav 
+else
+  echo "No secret provided."
+fi
