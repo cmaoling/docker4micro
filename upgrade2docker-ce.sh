70d1ecb20c05f348220985460b6adc87cb4ab1a9
Colinas Maoling <colinas.maoling@t-online.de>
2018-12-23 18:34:09 +0100

    Update upgrade2docker-ce.sh

--- upgrade2docker-ce.sh
+++ upgrade2docker-ce.sh
@@ -1,9 +1,10 @@
 # based on https://docs.docker.com/install/linux/docker-ce/ubuntu/
 echo "Upgrade from previous docker-io to docker-maintained docker-ce"
-apt-get remove docker docker-engine docker.io
+apt-get -y remove docker docker-engine docker.io
+apt-get -y autoremove
 docker --version
 echo "deb [arch=armhf] https://download.docker.com/linux/ubuntu jessie stable" > /etc/apt/sources.list.d/docker.list
 # cat /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get install docker-ce
+apt-get -y update
+apt-get -y install docker-ce
 docker --version
