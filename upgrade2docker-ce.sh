a73dd3c4abecb08b3bd869dab9ee89f7205a99cb
Colinas Maoling <colinas.maoling@t-online.de>
2018-12-23 14:08:39 +0100

    Create upgrade2docker-ce.sh
    initial reference script

---
+++ upgrade2docker-ce.sh
@@ -0,0 +1,9 @@
+# based on https://docs.docker.com/install/linux/docker-ce/ubuntu/
+echo "Upgrade from previous docker-io to docker-maintained docker-ce
+apt-get remove docker docker-engine docker.io
+docker --version
+echo "deb [arch=armhf] https://download.docker.com/linux/ubuntu jessie stable" > /etc/apt/sources.list.d/docker.list
+# cat /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install docker-ce
+docker --version
