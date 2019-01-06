9387a11edd883d683aa044de5b56c66b94bf3629
Colinas Maoling <colinas.maoling@t-online.de>
2019-01-06 16:26:39 +0100

    add mount of SATA disc

--- upgrade2docker-ce.sh
+++ upgrade2docker-ce.sh
@@ -12,3 +12,14 @@ apt-get -y install docker-ce
 docker --version
 #mkdir /sys/fs/cgroup/systemd
 #mount -t cgroup -o none,name=systemd cgroup /sys/fs/cgroup/systemd
+
+# command to read attached block devices:
+blkid
+mount -r /dev/sda6 /datalake
+cd /etc
+cat fstab
+cat /datalake/micro/fstab | grep sda >> fstab
+cat fstab
+cd /root
+cp -r /datalake/micro/my-ca .
+cp /datalake/micro/ca.pem .
